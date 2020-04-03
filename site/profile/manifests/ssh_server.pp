e1a80e34c3b16e4e8615d6db629ce93591d9674e
xabaweb <49246575+xabaweb@users.noreply.github.com>
2020-04-03 15:25:51 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => ' +2GMjddQybW1frVFMM18YVwaI93krpjyBz9r64SwCYbJBCzN7OS/DC7XCQPBDy2IFJw7y/jenLw6oMEXgFMEZFOSvSTuVJWZIDlVj1GY1BkzUogRFfL2JpcoBlnPSu6uUPRdSZp5nZsdp9NQzUFTGQxDW5hynhILJTB9jJEhgrqrSwPzR1HZe02rAEaGV4ex/QcHfMJ2OLj+OWfqo8IL7jtO1vQ8EYLu27OfnZFP3Zw3RqtHd58R/upP',
 	}  
 }
