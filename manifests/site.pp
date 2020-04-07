396f534aa02844b1a53699acb1aaeda46edfc34e
xabaweb <49246575+xabaweb@users.noreply.github.com>
2020-04-07 15:30:02 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,6 +9,9 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}\n",
   }
 }
+node 'minetest.puppet.vm' {
+  include role::minecrat_server
+}
 
 node /^web/ {
   include role::app_server
